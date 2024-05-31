7edf99c03e0271acd4757e02f5076d282a8200df
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-31 10:39:25 +0200

    import Meme from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <div>
      <Header />
+     <Meme />
     </div>
   )
 }
