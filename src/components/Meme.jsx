10c0aa7face2808d89804cf14abad3fa93a121ef
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-31 10:31:48 +0200

    Implemented Meme component containing a main element

---
+++ src/components/Meme.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Shut up"
+                        className="form--input"
+                    />    
+                </div>
+            </form>
+        </main>
+    )
+}
